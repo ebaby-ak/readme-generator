755851fe28859fdb93c5f3fd9cc56d195da64fad
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-07-16 15:58:44 -0500

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project',
+},
+{
+    type: 'input',
+    name: 'Table of Contents',
+    message: 'Enter a table of contents for your project',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions for your project',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage instructions for your project',
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'Enter the license for your project',
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter guidelines for contributing to your project',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions for your project',
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'Enter any questions or concerns you may have for your project',
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
